506afe0cb9a2d6950dc521511a500e0d76b2f4a0
Konstantin Mihaylov <konstantin.mihaylov.io@gmail.com>
2025-03-02 17:23:27 +0100

    Add meta tags and favicon in RootLayout component. Add Vercel logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,19 @@ const inter = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
     <html lang='en' suppressHydrationWarning>
+        <head>
+            <meta charSet='UTF-8' />
+            <link rel='icon' type='image/svg+xml' href='/vercel.svg' />
+            <meta
+                name='viewport'
+                content='width=device-width, initial-scale=1.0'
+            />
+            <title>Pixelator</title>
+        </head>
         <body className={inter.className}>
             <ThemeProvider
                 attribute='class'
-                defaultTheme='system'
+                defaultTheme='light'
                 enableSystem
                 disableTransitionOnChange
             >
